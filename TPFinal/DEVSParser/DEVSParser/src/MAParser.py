4b98a83ebe9a05e54ed61cba96cad499b13a0c74
Emiliano Lucero <eslucero2010@gmail.com>
2022-03-16 21:56:18 -0300

    sigo con el parser

--- TPFinal/DEVSParser/DEVSParser/src/MAParser.py
+++ TPFinal/DEVSParser/DEVSParser/src/MAParser.py
@@ -1,5 +1,3 @@
-import string
-
 import pyparsing as pp
 from enum import Enum
 
@@ -12,55 +10,194 @@ class MAParserError(Exception):
 
 class MAParser:
     def __init__(self):
-        keyword_begin_model_name = pp.Literal('[')
-        keyword_end_model_name = pp.Literal(']')
-        keyword_assign_attribute = pp.Keyword(':')
-        keyword_comment = pp.Keyword('%')
-        keyword_model_instance = pp.Literal('@')
-        keyword_link_at = keyword_model_instance
-        keyword_components = pp.CaselessKeyword("components")
-        keyword_in = pp.CaselessKeyword("in")
-        keyword_out = pp.CaselessKeyword("out")
-        keyword_link = pp.CaselessKeyword("link")
+        self.keyword_begin_model_name = pp.Literal('[')
+        self.keyword_end_model_name = pp.Literal(']')
+        self.keyword_assign_attribute = pp.Keyword(':')
+        self.keyword_comment = pp.Keyword('%')
+        self.keyword_model_instance = pp.Literal('@')
+        self.keyword_link_at = self.keyword_model_instance
+        self.keyword_components = pp.CaselessKeyword("components")
+        self.keyword_in = pp.CaselessKeyword("in")
+        self.keyword_out = pp.CaselessKeyword("out")
+        self.keyword_link = pp.CaselessKeyword("link")
 
-        value_component_atomic = pp.Word(pp.printables)
-        value_component_complex = pp.Word(pp.alphas)
-        value_component = pp.MatchFirst(value_component_atomic, value_component_complex)
-        word_components = keyword_components + keyword_assign_attribute + pp.OneOrMore(value_component)
+        self.value_component_atomic = pp.Word(pp.printables)
+        self.value_component_complex = pp.Word(pp.alphas)
+        self.value_component = pp.MatchFirst(self.value_component_atomic, self.value_component_complex)
+        self.word_components = self.keyword_components + self.keyword_assign_attribute + pp.OneOrMore(
+            self.value_component)
+        self.word_components.setParseAction(lambda s, l, t: ["COMPONENTS", *t[2:]])
 
-        word_in = keyword_in + keyword_assign_attribute + pp.OneOrMore(pp.Word(pp.printables))
-        word_out = keyword_out + keyword_assign_attribute + pp.OneOrMore(pp.Word(pp.printables))
+        self.word_in = self.keyword_in + self.keyword_assign_attribute + pp.OneOrMore(pp.Word(pp.printables))
+        self.word_in.setParseAction(lambda s, l, t: ["IN", *t[2:]])
+        self.word_out = self.keyword_out + self.keyword_assign_attribute + pp.OneOrMore(pp.Word(pp.printables))
+        self.word_out.setParseAction(lambda s, l, t: ["OUT", *t[2:]])
 
-        value_link_own = pp.Word(pp.printables)
-        value_link_other = pp.Word(pp.printables) + keyword_link_at + pp.Word(pp.printables)
-        value_link = pp.Or(value_link_own, value_link_other)
-        word_link = keyword_link + keyword_assign_attribute + pp.OneOrMore(value_link)
+        self.value_link_own = pp.Word(pp.printables)
+        self.value_link_other = pp.Word(pp.printables) + self.keyword_link_at + pp.Word(pp.printables)
+        self.value_link = pp.Or(self.value_link_own, self.value_link_other)
+        self.word_link = self.keyword_link + self.keyword_assign_attribute + pp.OneOrMore(self.value_link)
+        self.word_link.setParseAction(lambda s, l, t: ["LINK", *t[2:]])
 
-        word_user_attribute = pp.Word(pp.printables) + keyword_assign_attribute + pp.OneOrMore(pp.Word(pp.printables))
-        word_attribute = word_components("COMPONENTS") ^ word_in("IN") ^ word_out("OUT") ^ word_link("LINK") ^\
-                         word_user_attribute("ATTR")
+        self.word_user_attribute = pp.Word(pp.printables) + self.keyword_assign_attribute + pp.OneOrMore(
+            pp.Word(pp.printables))
+        self.word_user_attribute.setParseAction(lambda s, l, t: ["ATTR", t[0], *t[2:]])
+        self.word_attribute = self.word_components("COMPONENTS") ^ self.word_in("IN") ^ self.word_out(
+            "OUT") ^ self.word_link("LINK") ^ self.word_user_attribute("ATTR")
 
-        word_header = keyword_begin_model_name + pp.Word(pp.alphas) + keyword_end_model_name
-        word_comment = keyword_comment + pp.ZeroOrMore(pp.Word(pp.printables))
+        self.word_header = self.keyword_begin_model_name + pp.Word(pp.alphas) + self.keyword_end_model_name
+        self.word_header.setParseAction(lambda s, l, t: ["MODEL_DEF", t[1]])
+        self.word_comment = self.keyword_comment + pp.ZeroOrMore(pp.Word(pp.printables))
+        self.word_comment.setParseAction(lambda s, l, t: ["COMMENT", *t[1:]])
 
-        self.parser = word_comment("COMMENT") ^ word_header("DEFINITION") ^ word_attribute("ATTRIBUTE")
+        self.parser = self.word_comment("COMMENT") ^ self.word_header("DEFINITION") ^ self.word_attribute("ATTRIBUTE")
+        self._init_parse_dict_()
+
+    @staticmethod
+    def _create_context_():
+        """
+        Creates an empty context. A context stores information about MA file
+        and checks if its valid
+        """
+        context = {
+            "MODEL_DEFINITIONS": [],
+            "MODELS": {},
+            "CURRENT_MODEL": None
+        }
+        return context
+
+    @staticmethod
+    def _is_atomic_component_(name):
+        return name.find("@") != -1
+
+    @staticmethod
+    def _get_atomic_name_and_model_(name):
+        return name.split("@")
+
+    @staticmethod
+    def _create_model_structure_():
+        return {"IS_ATOMIC": True, "COMPONENTS": [], "IN": [], "OUT": [], "LINK": [], "ATTR": []}
+
+    def _parse_model_def_(self, data, context):
+        assert len(data) > 0, "Model definition statement is empty"
+
+        name = data[0]
+        context["MODEL_DEFINITIONS"].append(name)
+        context["CURRENT_MODEL"] = name
+        context["MODELS"][name] = self._create_model_structure_()
+        return context
+
+    def _parse_components_(self, data, context):
+        assert len(data) > 0, "Components statement is empty"
+
+        current_model = context["CURRENT_MODEL"]
+        for c in data:
+            if self._is_atomic_component_(c):
+                local_name, model_name = self._get_atomic_name_and_model_(c)
+                to_add = ("ATOMIC", local_name, model_name)
+            else:
+                to_add = ("COMPOSITE", c)
+            context["MODELS"][current_model]["COMPONENTS"].append(to_add)
+        context["MODELS"][current_model]["IS_ATOMIC"] = False
+        return context
+
+    @staticmethod
+    def _parse_in_(self, data, context):
+        assert len(data) > 0, "In statement has insufficient parameters"
+        current_model = context["CURRENT_MODEL"]
+        context["MODELS"][current_model]["IN"] = data
+        return context
+
+    @staticmethod
+    def _parse_out_(self, data, context):
+        assert len(data) > 0, "Out statement has insufficient parameters"
+        current_model = context["CURRENT_MODEL"]
+        context["MODELS"][current_model]["OUT"] = data
+        return context
+
+    def _parse_link_(self, data, context):
+        assert len(data) == 2, "Link statement has insufficient parameters"
+
+        current_model = context["CURRENT_MODEL"]
+        if self._is_atomic_component_(data[0]):
+            port_name, model = self._get_atomic_name_and_model_(data[0])
+            from_port = ("REMOTE_PORT", port_name, model)
+        else:
+            from_port = ("LOCAL_PORT", data[0])
+        if self._is_atomic_component_(data[1]):
+            port_name, model = self._get_atomic_name_and_model_(data[1])
+            to_port = ("REMOTE_PORT", port_name, model)
+        else:
+            to_port = ("LOCAL_PORT", data[1])
+
+        assert (from_port[0] != "REMOTE_PORT" or to_port[0] != "REMOTE_PORT"),\
+            "Link statement does not refer to local ports"
+
+        context["MODELS"][current_model]["LINK"].append((from_port, to_port))
+        return context
+
+    @staticmethod
+    def _parse_attr_(self, data, context):
+        assert len(data) > 1, "Attribute has insuficcient parameters"
+        current_model = context["CURRENT_MODEL"]
+        name = data[0]
+        values = data[1:]
+        context["MODELS"][current_model]["ATTR"].append((name, values))
+        return context
+
+    def _init_parse_dict_(self):
+        self.parse_dict = dict()
+        self.parse_dict["COMMENT"] = lambda data, context: context
+        self.parse_dict["MODEL_DEF"] = self._parse_model_def_
+        self.parse_dict["COMPONENTS"] = self._parse_components_
+        self.parse_dict["IN"] = self._parse_in_
+        self.parse_dict["OUT"] = self._parse_out_
+        self.parse_dict["LINK"] = self._parse_link_
+        self.parse_dict["ATTR"] = self._parse_attr_
 
     def parse_file(self, filename):
+        """Parses an entire MA file"""
         with open(filename, 'r') as f:
             lines = f.readlines()
             for line in lines:
                 print(self.scan_line(line))
 
     def scan_text(self, text):
+        """
+        Scans and tokenizes an entire text.
+        :parameter text: A string that contains the MA file text
+        :return: the scanned lines as a list
+        """
         res = []
         lines = text.splitlines()
         for line in lines:
-            res.append(self.scan_line(line))
+            scanned_line = self.scan_line(line)
+            if scanned_line is not None:
+                res.append(scanned_line)
         return res
 
     def scan_line(self, line):
+        """
+        Scans and tokenizes a line of text in MA language.
+        :parameter text: A string
+        :return: A token generated by the scanner
+        """
         try:
+            # Era solo espacio en blanco
+            if len(line.strip()) == 0:
+                return None
             return self.parser.parseString(line)
         except pp.ParseException as e:
             print(e.line)
             raise MAParserError
+
+    def parse_line(self, scanned_line, context):
+        """
+        Parses an already scanned line(a token) and adds it to the context
+        :param scanned_line: A token created as a result of scanning
+        :param context: Current parsing context
+        :return: a modified version of the context
+        """
+        token_type = scanned_line[0]
+        self.parse_dict[token_type](scanned_line[1:], context)
+        return context
--- TPFinal/DEVSParser/DEVSParser/test/test_MAParser.py
+++ TPFinal/DEVSParser/DEVSParser/test/test_MAParser.py
@@ -1,5 +1,6 @@
 from DEVSParser.src import MAParser
 import pytest
+from . import examples
 
 
 @pytest.fixture
@@ -7,59 +8,122 @@ def parser():
     return MAParser.MAParser()
 
 
+@pytest.fixture
+def context():
+    return MAParser.MAParser._create_context_()
+
+
+#
+# scan_line_tests
+#
+
+
 def test_scan_line_empty(parser):
-    with pytest.raises(MAParser.MAParserError) as e_info:
-        parser.scan_line("")
+    assert parser.scan_line("") is None
 
 
 def test_scan_line_comment_empty(parser):
-    assert parser.scan_line("%")["COMMENT"][0:1] == ["%"]
+    assert parser.scan_line("%")[0:1] == ["COMMENT"]
 
 
 def test_scan_line_comment_simple(parser):
-    assert parser.scan_line("% test comment")["COMMENT"][0:3] == ["%", "test", "comment"]
+    assert parser.scan_line("% test comment")[0:3] == ["COMMENT", "test", "comment"]
 
 
 def test_scan_line_simple_header(parser):
-    assert parser.scan_line("[ hello ]")["DEFINITION"][0:3] == ["[", "hello", "]"]
+    assert parser.scan_line("[ hello ]")[0:2] == ["MODEL_DEF", "hello"]
 
 
 def test_scan_line_simple_header_no_whitespace(parser):
-    assert parser.scan_line("[hello]")["DEFINITION"][0:3] == ["[", "hello", "]"]
+    assert parser.scan_line("[hello]")[0:2] == ["MODEL_DEF", "hello"]
+
+
+def test_scan_line_complex_header(parser):
+    assert parser.scan_line("[ helloWorld ]")[0:2] == ["MODEL_DEF", "helloWorld"]
 
 
 def test_scan_line_simple_components(parser):
-    assert parser.scan_line("components : test")["COMPONENTS"][0:3] == ["components", ":", "test"]
+    assert parser.scan_line("components : test")[0:3] == ["COMPONENTS", "test"]
 
 
 def test_scan_line_complex_components(parser):
-    assert parser.scan_line("components : test@generator")["COMPONENTS"][0:3] == ["components", ":", "test@generator"]
+    assert parser.scan_line("components : test@generator")[0:3] == ["COMPONENTS", "test@generator"]
 
 
 def test_scan_line_simple_in(parser):
-    assert parser.scan_line("in : port")["IN"][0:3] == ["in", ":", "port"]
+    assert parser.scan_line("in : port")[0:3] == ["IN", "port"]
 
 
 def test_scan_line_complex_in(parser):
-    assert parser.scan_line("in : port1 port2")["IN"][0:4] == ["in", ":", "port1", "port2"]
+    assert parser.scan_line("in : port1 port2")[0:4] == ["IN", "port1", "port2"]
 
 
 def test_scan_line_simple_out(parser):
-    assert parser.scan_line("out : port")["OUT"][0:3] == ["out", ":", "port"]
+    assert parser.scan_line("out : port")[0:3] == ["OUT", "port"]
 
 
 def test_scan_line_complex_out(parser):
-    assert parser.scan_line("out : port1 port2")["OUT"][0:4] == ["out", ":", "port1", "port2"]
+    assert parser.scan_line("out : port1 port2")[0:4] == ["OUT", "port1", "port2"]
 
 
 def test_scan_line_outgoing_link(parser):
-    assert parser.scan_line("link : port port@test")["LINK"][0:4] == ["link", ":", "port", "port@test"]
+    assert parser.scan_line("link : port port@test")[0:4] == ["LINK", "port", "port@test"]
 
 
 def test_scan_line_incoming_link(parser):
-    assert parser.scan_line("link : port@test port ")["LINK"][0:4] == ["link", ":", "port@test", "port"]
+    assert parser.scan_line("link : port@test port ")[0:4] == ["LINK", "port@test", "port"]
 
 
 def test_scan_line_user_attribute(parser):
-    assert parser.scan_line("distribution : normal")["ATTRIBUTE"][0:3] == ["distribution", ":", "normal"]
+    assert parser.scan_line("distribution : normal")[0:3] == ["ATTR", "distribution", "normal"]
 
+
+#
+# scan_text_tests
+#
+def test_scan_text_example_simple(parser):
+    scanned_text = parser.scan_text(examples.EXAMPLE_SIMPLE)
+    assert len(scanned_text) == 12
+    assert scanned_text[0][0:2] == ["MODEL_DEF", "top"]
+    assert scanned_text[1][0:2] == ["COMPONENTS", "generator@generator"]
+    assert scanned_text[2][0:2] == ["OUT", "out_port"]
+    assert scanned_text[3][0:2] == ["IN", "stop"]
+    assert scanned_text[4][0:3] == ["LINK", "stop", "stop@generator"]
+    assert scanned_text[5][0:3] == ["LINK", "out@generator", "out_port"]
+    assert scanned_text[6][0:2] == ["MODEL_DEF", "generator"]
+    assert scanned_text[7][0:3] == ["ATTR", "distribution", "normal"]
+    assert scanned_text[8][0:3] == ["ATTR", "mean", "3"]
+    assert scanned_text[9][0:3] == ["ATTR", "deviation", "1"]
+    assert scanned_text[10][0:3] == ["ATTR", "initial", "1"]
+    assert scanned_text[11][0:3] == ["ATTR", "increment", "5"]
+
+#
+# parse_line_tests
+#
+
+
+def test_parse_line_comment(parser, context):
+    """Parsing a comment does not affect the context"""
+    context = parser.parse_line(parser.scan_line("%"), context)
+    assert not context["MODEL_DEFINITIONS"]
+    assert not context["MODELS"]
+    assert not context["CURRENT_MODEL"]
+
+
+def test_parse_line_model_def(parser, context):
+    """
+    Parsing a model definition adds a new model to the context and
+    sets it as the current one.
+    """
+    context = parser.parse_line(parser.scan_line("[test]"), context)
+    assert "test" in context["MODEL_DEFINITIONS"]
+    assert context["CURRENT_MODEL"] == "test"
+    assert context["MODELS"]["test"]
+
+
+def test_parse_line_components(parser, context):
+    """
+    Parsing a model definition adds a new model to the context and
+    sets it as the current one.
+    """
+    pass
